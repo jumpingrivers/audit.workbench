afc1e3adb40088575bda727779ddfbb60ce69d08
Shane Halloran <shane@jumpingrivers.com>
2022-08-18 16:10:53 +0100

    Commenting out some tests which we're not working on a the moment

--- tests/tests.R
+++ tests/tests.R
@@ -8,48 +8,48 @@
 test_that("Check if R calculation works", {
   expect_equal(2 + 2, 4)
 })
-
-# Testing if Python via Terminal works
-test_that("Check if Python calculation via Terminal works", {
-  expect_equal(system('python3 -c "print(2+2)"', intern = TRUE), "4")
-})
-
-# Testing if Python via Reticulate works
-test_that("Check if Python works via Reticulate",
-          expect_equal({
-            install.packages('reticulate')
-            library(reticulate)
-            py_run_string('x=2+2')
-            py$x
-          }, 4))
-
-# Testing if Git pull works
-test_that("Checking if Git pull works",
-  expect_equal({
-    testing_git_clone_directory_name = paste('git_clone_', sub(" ", "_", Sys.time()), sep ='')
-    git_terminal_command = paste(
-      "git clone",
-      "https://github.com/jumpingrivers/diffify.git",
-      testing_git_clone_directory_name,
-      "&& ls")
-    working_directory_contents_after_git_pull = system(git_terminal_command, intern = TRUE)
-    testing_git_clone_directory_name %in% working_directory_contents_after_git_pull},
-    TRUE
-  ))
-#system("rm -rf testing_git_clone")
-
-# TODO Git push, which repo to push to?
-
-# Installing Tidyverse
-test_that("Check if installing popular R package {tidyverse} works.",
-          {
-            expect_equal({
-              install.packages('tidyverse')
-              library(ggplot2)
-              library(dplyr)
-              "tidyverse" %in% installed.packages()[, "Package"]
-            }, TRUE)
-          })
+# 
+# # Testing if Python via Terminal works
+# test_that("Check if Python calculation via Terminal works", {
+#   expect_equal(system('python3 -c "print(2+2)"', intern = TRUE), "4")
+# })
+# 
+# # Testing if Python via Reticulate works
+# test_that("Check if Python works via Reticulate",
+#           expect_equal({
+#             install.packages('reticulate')
+#             library(reticulate)
+#             py_run_string('x=2+2')
+#             py$x
+#           }, 4))
+# 
+# # Testing if Git pull works
+# test_that("Checking if Git pull works",
+#   expect_equal({
+#     testing_git_clone_directory_name = paste('git_clone_', sub(" ", "_", Sys.time()), sep ='')
+#     git_terminal_command = paste(
+#       "git clone",
+#       "https://github.com/jumpingrivers/diffify.git",
+#       testing_git_clone_directory_name,
+#       "&& ls")
+#     working_directory_contents_after_git_pull = system(git_terminal_command, intern = TRUE)
+#     testing_git_clone_directory_name %in% working_directory_contents_after_git_pull},
+#     TRUE
+#   ))
+# #system("rm -rf testing_git_clone")
+# 
+# # TODO Git push, which repo to push to?
+# 
+# # Installing Tidyverse
+# test_that("Check if installing popular R package {tidyverse} works.",
+#           {
+#             expect_equal({
+#               install.packages('tidyverse')
+#               library(ggplot2)
+#               library(dplyr)
+#               "tidyverse" %in% installed.packages()[, "Package"]
+#             }, TRUE)
+#           })
 
 #TODO: install Python packages numpy and pandas
 
