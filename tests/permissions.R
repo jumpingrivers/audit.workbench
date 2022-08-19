e70e38cf84a2c6e47ed622f7fd20c9de8280f53e
Shane Halloran <shane@jumpingrivers.com>
2022-08-19 10:12:17 +0100

    Splitting tests into smaller files

---
+++ tests/permissions.R
@@ -0,0 +1,9 @@
+#TODO: install a popular Bioconductor package
+
+#TODO: install an R package based on a Git repository
+
+#TODO: append a line (comment) to .Rprofile, and check that it has been modified
+
+#TODO: same with .Renviron
+
+#TODO: test creating a new file in the home directory
---
+++ tests/technical_publishing.R
@@ -0,0 +1,5 @@
+#TODO qmd->HTML
+
+#TODO qmd->PDF (implicitly also tests for LaTeX)
+
+#TODO: Jupyter notebook: built-in verify-installation check
