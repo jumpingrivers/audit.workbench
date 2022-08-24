997aa6d48fbe50cc07e337c472b7dd5bafdfabbb
Shane Halloran <shane@jumpingrivers.com>
2022-08-24 13:38:43 +0100

    Using withr::with_dir for creating directory in Git pull test

--- tests/testthat/test-languages_and_packages.R
+++ tests/testthat/test-languages_and_packages.R
@@ -26,23 +26,47 @@ testthat::test_that("Python via Reticulate", {
 
 # Testing if Git pull works
 testthat::test_that("Git pull", {
-  testthat::skip("Need to fix git pull test")
-  testing_git_clone_directory_name = file.path(tempdir(), "git_clones", Sys.time())
+  # URL of Git repository which we would like to test cloning of:
+  GIT_CLONE_URL = "https://github.com/jumpingrivers/diffify.git"
+  
+  #Current time, with spaces removed:
+  current_time_no_spaces = gsub(" ", "", Sys.time())
+  # Variable representing the path of the directory where we want git to save the
+  # cloned files on our machine:
+  temporary_directory_which_will_contain_git_clone_directory = file.path(tempdir(), "git_clones")
+  # Variable representing the name of the new subdirectory we would like to
+  # clone into. This doesn't include the names of parent folders:
+  testing_git_clone_directory_name = paste("git_clones", current_time_no_spaces, sep="")
+  # The full path of the directory we would like to clone into:
+  git_clone_directory_path = file.path(
+    temporary_directory_which_will_contain_git_clone_directory,
+    testing_git_clone_directory_name
+  )
+  
+  # Constructing the terminal command to clone our Git repository and then list
+  # all files in the containing directory:
   git_terminal_command = paste(
     "git clone",
     "https://github.com/jumpingrivers/diffify.git",
-    testing_git_clone_directory_name,
-    "&& ls"
+    git_clone_directory_path,
+    "&& ls",
+    temporary_directory_which_will_contain_git_clone_directory
   )
-  withr::with_tempdir({
-    working_directory_contents_after_git_pull = system(git_terminal_command, intern = TRUE)
+  if(!dir.exists(temporary_directory_which_will_contain_git_clone_directory)){dir.create(temporary_directory_which_will_contain_git_clone_directory)}
+  # withr::with_tempdir creates a temporary directory, which will be deleted when
+  # the session ends:
+  withr::with_dir(temporary_directory_which_will_contain_git_clone_directory, {
+    # Run the terminal command which clones the Git repository and returns the
+    # list of all files in the containing directory:
+    containing_directory_contents_after_git_pull = system(git_terminal_command, intern = TRUE)
+    # Assert that the cloned repository is where we expect it to be:
+    print(paste("We are checking this directory contents listing:", list.files(getwd())))
+    print(paste("For these contents", testing_git_clone_directory_name))
     testthat::expect_true(
-      testing_git_clone_directory_name %in% working_directory_contents_after_git_pull
+      testing_git_clone_directory_name %in% list.files(getwd())
     )
-  }, tmpdir = testing_git_clone_directory_name)
-  # TODO: delete git_clone_* directories afterwards. This approach? https://withr.r-lib.org/reference/with_tempfile.html#ref-usage
+  })
 })
-#system("rm -rf testing_git_clone")
 
 # TODO Git push, which repo to push to?
 
