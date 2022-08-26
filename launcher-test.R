1b131dc0f9f67c73ff3daa06c83eb1a6fefd823c
Rhian Davies <rhian@jumpingrivers.com>
2022-08-26 14:46:30 +0100

    Tidy up

--- launcher-test.R
+++
@@ -1,7 +0,0 @@
-# Run as a Launcher Job
-logr::log_open('launcher.log')
-Sys.sleep(30)
-logr::log_print('thirty seconds have passed')
-Sys.sleep(30)
-logr::log_print('one minute has passed')
-logr::log_close()
