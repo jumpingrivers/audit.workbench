721adf074b9e2e1480fab6e0a8c228fcfa7df1e5
Rhian Davies <rhian@jumpingrivers.com>
2022-08-31 10:25:57 +0100

    Refactor R pkg tests

---
+++ tests/testthat/test-r.R
@@ -0,0 +1,41 @@
+repo_url = "https://packagemanager.rstudio.com/cran/__linux__/focal/latest"
+pkgs = rownames(installed.packages())
+
+test_that("Check if R calculation works", {
+  expect_equal(2 + 2, 4)
+})
+
+
+test_that("Installing an R package", {
+  install.packages("lubridate", repo = repo_url)
+  withr::defer(remove.packages("lubridate"))
+  expect_true(require("lubridate"))
+})
+
+bioconductor_install = function() {
+  if (!("BiocManager" %in% pkgs)) {
+    install.packages("BiocManager", repo = repo_url)
+    withr::defer(remove.packages("BiocManager"))
+  }
+  BiocManager::install("graph", force = TRUE)
+  withr::defer(remove.packages("graph"))
+  return(require(graph))
+}
+
+github_install = function() {
+  if (!("devtools" %in% pkgs)) {
+    install.packages("devtools", repo = repo_url)
+    withr::defer(remove.packages("devtools"))
+  }
+  devtools::install_github("jumpingrivers/datasauRus")
+  withr::defer(remove.packages("datasauRus"))
+  return(require(datasauRus))
+}
+
+test_that("Bioconductor installation works", {
+  expect_true(bioconductor_install())
+})
+
+test_that("GitHub R package installation works", {
+    expect_true(github_install())
+})
--- tests/testthat/test-r_related.R
+++ tests/testthat/test-r_related.R
@@ -1,45 +1,38 @@
 repo_url = "https://packagemanager.rstudio.com/cran/__linux__/focal/latest"
-r_package_to_install = "drat"
 
-# Testing if R Console calculation
-testthat::test_that("Check if R calculation works", {
-  testthat::expect_equal(2 + 2, 4)
+test_that("Check if R calculation works", {
+  expect_equal(2 + 2, 4)
 })
 
-#Installing an R package
-testthat::test_that(paste(
-  "Installing R package: {",
-  r_package_to_install,
-  "}",
-  sep = ""
-),
-{
-  install.packages(r_package_to_install,
-                   repo = repo_url,
-                   quietly = TRUE)
+
+test_that("Installing an R package", {
+  install.packages("drat", repo = repo_url)
+  withr::defer(remove.packages("drat"))
+  expect_true(require(graph))x
+
     package_has_loaded = withr::with_package(package = r_package_to_install, {
-      testthat::expect_true(r_package_to_install %in% installed.packages()[, "Package"])
+      expect_true(r_package_to_install %in% installed.packages()[, "Package"])
     },
     logical.return = TRUE
   )
-  testthat::expect_true(package_has_loaded)
-  testthat::expect_true(require(r_package_to_install, character.only = TRUE))
-  testthat::expect_true(r_package_to_install %in% installed.packages()[, "Package"])
+  expect_true(package_has_loaded)
+  expect_true(require(r_package_to_install, character.only = TRUE))
+  expect_true(r_package_to_install %in% installed.packages()[, "Package"])
 })
 
 # Installing a Bioconductor package
-testthat::test_that("Bioconductor installation works", {
+test_that("Bioconductor installation works", {
   if (require(BiocManager)) {
     BiocManager::install("graph", force = TRUE)
   } else {
     install.packages("BiocManager", repo = repo_url)
     BiocManager::install("graph", force = TRUE)
   }
-  testthat::expect_true(require(graph))
+  expect_true(require(graph))
 })
 
 # Installing an R package from GitHub
-testthat::test_that("GitHub R package installation works", {
+test_that("GitHub R package installation works", {
   devtools::install_github("jumpingrivers/datasauRus")
-  testthat::expect_true(require(datasauRus))
+  expect_true(require(datasauRus))
 })
