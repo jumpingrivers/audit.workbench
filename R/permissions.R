7aa14dfaf994008abd82fb2571a745b698fc122f
Shane Halloran <shane@jumpingrivers.com>
2022-09-05 16:34:27 +0100

    Removed normalizePath for inputs to file.mode, since file.mode doesn't need that

--- R/permissions.R
+++ R/permissions.R
@@ -5,10 +5,8 @@ touch_file_succeeds = function(filepath) {
 }
 
 get_permissions_of_file = function(filepath) {
-  # We use normalisePath for tilde-expansion, since processx doesn't do this:
-  normalized_filepath = normalizePath(filepath)
   # The command to check, in octal form, what are the permissions of the file:
-  permissions_on_file = file.mode(normalized_filepath)
+  permissions_on_file = file.mode(filepath)
   # Convert the string to an integer, so that we can use it to make comparisons:
   permissions_on_file_int = strtoi(permissions_on_file)
   return(permissions_on_file_int)
