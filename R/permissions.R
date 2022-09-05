70d2459a2b1db7c974e573507b6a4ca7d31b4fde
Shane Halloran <shane@jumpingrivers.com>
2022-09-05 16:31:37 +0100

    Using file.mode to get file permissions, instead of stat through processx::run

--- R/permissions.R
+++ R/permissions.R
@@ -6,14 +6,9 @@ touch_file_succeeds = function(filepath) {
 
 get_permissions_of_file = function(filepath) {
   # We use normalisePath for tilde-expansion, since processx doesn't do this:
-  file_location = normalizePath(filepath)
+  normalized_filepath = normalizePath(filepath)
   # The command to check, in octal form, what are the permissions of the file:
-  check_permission = processx::run("stat", args = c("-c", "'%a", file_location))
-  # Obtain the stdout of the command:
-  stdout_of_check_permission = check_permission$stdout
-  # Remove any whitespace, newlines or quotes from the stdout string:
-  permissions_on_file = gsub(pattern = "\\s|'", "", stdout_of_check_permission)
-  # Convert the string to an integer, so that we can use it to make comparisons
+  permissions_on_file = file.mode(normalized_filepath)
   permissions_on_file_int = strtoi(permissions_on_file)
   return(permissions_on_file_int)
 }
