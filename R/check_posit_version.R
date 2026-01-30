6c81055c6dcf915ef18bfb1a9690cced2c5977e5
Colin Gillespie <csgillespie@gmail.com>
2026-01-30 14:27:59 +0000

    fix: Version parsing

--- R/check_posit_version.R
+++ R/check_posit_version.R
@@ -4,9 +4,13 @@ check_posit_version = function() {
   } else {
     cmd = system2("rstudio-server", args = "version", stdout = TRUE)
     posit_version = stringr::str_remove(cmd, " .*")
-
   }
-  posit_version = stringr::str_remove(posit_version, "\\+[0-9]*")
+  posit_version = stringr::str_squish(posit_version)
+
+  posit_version = stringr::str_extract(
+    posit_version,
+    "^20[0-9][0-9]\\.[0-9]?[0-9]\\.[0-9]?[0-9]"
+  )
   audit.base::audit_posit_version(posit_version, type = "workbench")
   return(posit_version)
 }
